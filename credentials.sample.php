f99aaaa782320f0e51851375a3900cb0e56c4b15
stephan lindauer <stephan.lindauer@gmail.com>
2015-12-18 00:09:07 +0100

    credentials sample and actual credentials file in gitignore

---
+++ credentials.sample.php
@@ -0,0 +1,13 @@
+<?php
+
+// database
+define("DATABASE_HOST", "XXX");
+define("DATABASE_NAME", "XXX");
+define("DATABASE_USER", "XXX");
+define("DATABASE_PASSWORD", "XXX");
+
+// twitter
+define('TWITTER_OAUTH_ACCESS_TOKEN', "XXX");
+define('TWITTER_OAUTH_ACCESS_TOKEN_SECRET', "XXX");
+define('TWITTER_CONSUMER_KEY', "XXX");
+define('TWITTER_CONSUMER_SECRET', "XXX");
